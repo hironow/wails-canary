33f739eff0e36dac4b013e68544ed9857f32ffa7
hironow <hironow365@gmail.com>
2022-11-03 00:28:11 +0900

    dogs api

--- app.go
+++ app.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sort"
 )
 
 // App struct
@@ -21,7 +26,76 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+// RandomImage get result
+type RandomImage struct {
+	Message string
+	Status  string
+}
+
+// AllBreeds get result
+type AllBreeds struct {
+	Message map[string]map[string][]string
+	Status  string
+}
+
+// ImagesByBreed get result
+type ImagesByBreed struct {
+	Message []string
+	Status  string
+}
+
+func (a *App) GetRandomImageUrl() string {
+	res, err := http.Get("https://dog.ceo/api/breeds/image/random")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resData, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data RandomImage
+	json.Unmarshal(resData, &data)
+	return data.Message
+}
+
+func (a *App) GetBreedList() []string {
+	var breeds []string
+
+	res, err := http.Get("https://dog.ceo/api/breeds/list/all")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resData, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data AllBreeds
+	json.Unmarshal(resData, &data)
+
+	for k := range data.Message {
+		breeds = append(breeds, k)
+	}
+	sort.Strings(breeds)
+	return breeds
+}
+
+func (a *App) GetImageUrlsByBreed(breed string) []string {
+	url := fmt.Sprintf("%s%s%s%s", "https://dog.ceo/api/", "breed/", breed, "/images")
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resData, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data ImagesByBreed
+	json.Unmarshal(resData, &data)
+	return data.Message
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,79 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { useEffect, useState, ChangeEvent } from "react";
+import "./App.css";
+import {
+  GetBreedList,
+  GetImageUrlsByBreed,
+  GetRandomImageUrl,
+} from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+  const [randomImageUrl, setRandomImageUrl] = useState("");
+  const [breeds, setBreeds] = useState<string[]>([]);
+  const [photos, setPhotos] = useState<string[]>([]);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+  const [selectedBreed, setSelectedBreed] = useState<string>("");
+  const updateSelectedBreed = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedBreed(event.target.value);
+  };
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+  const [showRandomPhoto, setShowRandomPhoto] = useState(false);
+  const [showBreedPhotos, setShowBreedPhotos] = useState(false);
+
+  useEffect(() => {
+    getBreedList();
+  }, []);
+
+  function getRandomImageUrl() {
+    setShowRandomPhoto(false);
+    setShowBreedPhotos(false);
+    GetRandomImageUrl().then((result) => setRandomImageUrl(result));
+    setShowRandomPhoto(true);
+  }
+
+  function getBreedList() {
+    GetBreedList().then((result) => setBreeds(result));
+  }
+
+  function getImageUrlsByBreed() {
+    setShowRandomPhoto(false);
+    setShowBreedPhotos(false);
+    GetImageUrlsByBreed(selectedBreed).then((result) => setPhotos(result));
+    setShowBreedPhotos(true);
+  }
+
+  return (
+    <div id="App">
+      <div>
+        <button className="btn" onClick={getRandomImageUrl}>
+          Fetch a dog randomly
+        </button>
+        <p>Click on down arrow to select a breed</p>
+        <select
+          title="select-breed"
+          value={selectedBreed}
+          onChange={updateSelectedBreed}
+        >
+          {breeds?.map((breed, i) => (
+            <option key={i} value={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
+        <button className="btn" onClick={getImageUrlsByBreed}>
+          Fetch by this breed
+        </button>
+      </div>
+
+      {showRandomPhoto && (
+        <img id="random-photo" src={randomImageUrl} alt="No dog found" />
+      )}
+
+      {showBreedPhotos &&
+        photos?.map((photo, i) => (
+          <img key={i} id="breed-photos" src={photo} alt="No dog found" />
+        ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
